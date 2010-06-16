1088dc04c684bda819c0c2af20caf20d8dfed763
altay <aguvench@gmail.com>
2010-06-16 10:16:47 -0700

    some tests, and require httparty+json gems

--- test/test_wordnik-ruby.rb
+++ test/test_wordnik-ruby.rb
@@ -1,7 +1,40 @@
 require 'helper'
 
 class TestWordnikRuby < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+
+  should "raise InvalidApiKeyError if no api key specified in initializer" do
+    assert_raises(InvalidApiKeyError) do
+      Wordnik.new
+    end
   end
+
+  context "a valid api key" do
+    setup do
+      @api_key = "85b993ddaabe04346e0090d379b02d18ad04bda75d4e0ecca"
+    end
+
+    should "instantiate a Wordnik object without authentication" do
+      w = Wordnik.new({:api_key=>@api_key})
+      assert_equal w.class, Wordnik
+      assert_equal w.api_key, @api_key
+      assert w.auth_token.nil?
+      assert w.user_id.nil?
+    end
+
+    context "a valid, unauthenticated Wordnik object" do
+      setup do
+        @w = Wordnik.new({:api_key=>@api_key})
+      end
+
+      should "raise InvalidAuthTokenError for api methods that require authentication, if no valid auth_key" do
+        assert_raises(InvalidAuthTokenError){ @w.lists }
+        assert_raises(InvalidAuthTokenError){ @w.create_list("testlist", "testdescription") }
+      end
+
+      should "" do
+      end
+    end
+
+  end
+
 end
--- wordnik-ruby.gemspec
+++ wordnik-ruby.gemspec
@@ -5,7 +5,7 @@
 
 Gem::Specification.new do |s|
   s.name = %q{wordnik-ruby}
-  s.version = "0.1.0"
+  s.version = "0.1.1"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Altay Guvench"]
@@ -48,11 +48,17 @@ Gem::Specification.new do |s|
 
     if Gem::Version.new(Gem::VERSION) >= Gem::Version.new('1.2.0') then
       s.add_development_dependency(%q<thoughtbot-shoulda>, [">= 0"])
+      s.add_runtime_dependency(%q<httparty>, [">= 0"])
+      s.add_runtime_dependency(%q<json>, [">= 0"])
     else
       s.add_dependency(%q<thoughtbot-shoulda>, [">= 0"])
+      s.add_dependency(%q<httparty>, [">= 0"])
+      s.add_dependency(%q<json>, [">= 0"])
     end
   else
     s.add_dependency(%q<thoughtbot-shoulda>, [">= 0"])
+    s.add_dependency(%q<httparty>, [">= 0"])
+    s.add_dependency(%q<json>, [">= 0"])
   end
 end
 
